2df33229e5dc33c46d6587e6ce6d590d8505f392
Hamilton Rodrigues <hamiltonventtura@gmail.com>
2024-03-14 08:49:04 -0500

    curso Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tarefasof';
+ public title = signal("Bem vindo a suas tarefas");
+ public subtitulo = signal("subtitulos");
+ public nome = signal ("hamilton");
+
+ //ele vai computar e ficar observando se ouver alguma mudança ele irá atualizar os dados
+ // ele computa as alterações
+
+ public tudo = computed( ()=>{
+    return this.title() + this.subtitulo();
+ })
+
+ updateTitulo(){
+    return this.title.set("João");
+ }
+
+ updateSubTitulo(){
+  return this.subtitulo.set("Analista de Sistemas");
+ }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:"", component:HomeComponent},
+{path:"perfil", component:PerfilComponent},
+
+
+
+];
